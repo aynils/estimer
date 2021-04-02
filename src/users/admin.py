b183794f4ad813951ab5ea68bd1b8ab71c788cca
Seraphin <seraphin@aynils.ca>
2021-04-02 16:25:22 -0300

    ADMIN - Register Custom user to admin pannel

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CustomUser
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("email", "last_login")
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
