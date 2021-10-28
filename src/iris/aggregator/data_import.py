6c09b2c03bb63ae584cafee0808d8c727670922d
ExoCinki <aolacot@gmail.com>
2021-10-28 13:38:56 +0200

    Download Iris [OK]

--- src/iris/aggregator/data_import.py
+++ src/iris/aggregator/data_import.py
@@ -4,16 +4,42 @@ from django.contrib.gis.utils import LayerMapping
 
 from iris.models import IRIS
 
+import requests
+import os.path
+from pathlib import Path
+
+from pyunpack import Archive
+
 logger = logging.getLogger(__name__)
 
 IGN_SRID = "epsg:2154"  # Lambert 93
 
+folder_path = str(Path.home())
+
+
+def build_url(year: int) -> str:
+    return f"http://data.cquest.org/ign/contours-iris/CONTOURS-IRIS_2-1__SHP__FRA_{year}-01-01.7z"
+
+
+def download_shp(url: str, folder_path: str) -> str:
+    local_filename = f"{url.split('/')[-1]}"
+    if not os.path.exists(f"{folder_path}/{local_filename}"):
+        with requests.get(url, stream=True) as r:
+            r.raise_for_status()
+            with open(f"{folder_path}/{local_filename}", "wb") as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    f.write(chunk)
+    else:
+        print("File already existing. Skip download")
+
+    return local_filename
+
 
 # TODO: add script to download this file from the server
 SHP_INPUT_PATH = (
-    "/Users/seraphinvandegar/Downloads/CONTOURS-IRIS_2-1__SHP__"
-    "FRA_2018-01-01/CONTOURS-IRIS/1_DONNEES_LIVRAISON_2018-07-00057/"
-    "CONTOURS-IRIS_2-1_SHP_LAMB93_FXX-2018"
+    "/home/cink/Téléchargements/CONTOURS-IRIS_2-1__SHP__FRA_2020-01-01/"
+    "CONTOURS-IRIS/1_DONNEES_LIVRAISON_2020-12-00282/"
+    "CONTOURS-IRIS_2-1_SHP_LAMB93_FXX-2020"
 )
 
 
