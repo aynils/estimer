a01b5754036c54e533b7c55518a9a436b5770b14
ExoCinki <aolacot@gmail.com>
2022-02-09 16:18:22 +0100

    Rename Function on agencies/data/calculator

--- src/agencies/data/calculator.py
+++ src/agencies/data/calculator.py
@@ -1,8 +1,7 @@
 from typing import List
 
-from src.agencies.models import Agency
 from src.dvf.models import Commune
 
 
-def get_cities_owned_by_agencies() -> List[Commune]:
+def get_cities_not_owned_by_agencies() -> List[Commune]:
     return Commune.objects.all().filter(agency_id__isnull=True).values("code_postal", "nom_commune", "code_commune")
--- src/agencies/views.py
+++ src/agencies/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
-from src.agencies.data.calculator import get_cities_owned_by_agencies
+from src.agencies.data.calculator import get_cities_not_owned_by_agencies
 
 
 def agency(request):
-    test = get_cities_owned_by_agencies()
+    test = get_cities_not_owned_by_agencies()
     context = {"test": test}
     return render(request, "agencies/agency.html", context)
