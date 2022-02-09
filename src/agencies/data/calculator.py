799d9b2ff6530ea6389cd6a0edde39de73e9f1b4
ExoCinki <aolacot@gmail.com>
2022-02-09 20:23:27 +0100

    Cities With Pricing Finished

--- src/agencies/data/calculator.py
+++ src/agencies/data/calculator.py
@@ -1,26 +1,60 @@
 from typing import List
 
-
+from src.agencies.models import Pricing
 from src.dvf.models import Commune
 from src.population.models import PopulationStat
 
 
 def get_cities_not_owned_by_agencies() -> List[Commune]:
-    return Commune.objects.all().filter(agency_id__isnull=True).values("code_postal", "nom_commune", "code_commune")
+    cities = Commune.objects.all().filter(agency_id__isnull=True).values("code_postal", "nom_commune", "code_commune")
+    return cities
 
 
-def get_pricing_for_cities():
+def add_population_to_cities():
     cities = get_cities_not_owned_by_agencies()
     population = PopulationStat.objects.all().values("code_commune", "total_population")
+    population = {populate.get("code_commune"): populate for populate in population}
     cities_with_population = []
     for city in cities:
-        for populate in population:
-            if city.get("code_commune") == populate.get("code_commune"):
-                city_with_population = {
-                    "code_commune": city.get("code_commune"),
-                    "nom_commune": city.get("nom_commune"),
-                    "total_population": populate.get("total_population"),
-                }
-                cities_with_population.append(city_with_population)
-                break
+        code_commune = city.get("code_commune")
+        total_population = population.get(code_commune, {}).get("total_population")
+        city_with_population = {
+            "code_commune": code_commune,
+            "nom_commune": city.get("nom_commune"),
+            "total_population": total_population,
+        }
+        cities_with_population.append(city_with_population)
+
     return cities_with_population
+
+
+def add_pricing_to_cities():
+    global total_population
+    cities = add_population_to_cities()
+    pricing = Pricing.objects.all().values("min_population", "max_population", "pricing")
+    cities_with_pricing = []
+    for city in cities:
+        code_commune = city.get("code_commune")
+        total_population = city.get("total_population")
+        if total_population is not None:
+            total_population = int(city.get("total_population"))
+        else:
+            total_population = 0
+        for price in pricing:
+            population_min = price.get("min_population")
+            population_max = price.get("max_population")
+            if total_population != 0:
+                if total_population in range(population_min, population_max):
+                    city_with_pricing = {
+                        "code_commune": code_commune,
+                        "nom_commune": city.get("nom_commune"),
+                        "pricing": price.get("pricing"),
+                    }
+                    cities_with_pricing.append(city_with_pricing)
+
+    return cities_with_pricing
+
+
+def get_cities_with_pricing():
+    cities_pricing = add_pricing_to_cities()
+    return cities_pricing
--- src/agencies/views.py
+++ src/agencies/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
-from src.agencies.data.calculator import get_pricing_for_cities
+from src.agencies.data.calculator import get_cities_with_pricing
 
 
 def agency(request):
-    test = get_pricing_for_cities()
-    context = {"test": test}
+    cities_pricing = get_cities_with_pricing()
+    context = {"cities_pricing": cities_pricing}
     return render(request, "agencies/agency.html", context)
