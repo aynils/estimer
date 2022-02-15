0bd99a45bda9bf989b27d721fec03b38eeed9f49
Seraphin <svandegar@hotmail.com>
2022-02-15 09:13:37 -0400

    DESIGN - AGENCY - Fix Mobile

--- src/dvf/data/cities.py
+++ src/dvf/data/cities.py
@@ -321,7 +321,7 @@ def get_agent(code_commune: str) -> Agent:
 
     except:
         agent = Agent(
-            picture="https://estimer.com/static/images/logo/logo_round.svg",
+            picture="https://estimer.com/static/images/logo/logo_round.png",
             name="Olivier Pourquier",
             agency="estimer.com",
             description="""Vous souhaitez obtenir une estimation précise de votre bien ?
