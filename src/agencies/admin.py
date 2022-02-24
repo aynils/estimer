b8dd64aeca3d836bbba41f34687b0d497001e7ed
Seraphin <svandegar@hotmail.com>
2022-02-24 16:22:32 -0400

    ADMIN - Add pricings to Django admin portal

--- src/agencies/admin.py
+++ src/agencies/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Agency
+from .models import Agency, Pricing
 
 
 class AgencyAdmin(admin.ModelAdmin):
@@ -8,4 +8,10 @@ class AgencyAdmin(admin.ModelAdmin):
     list_filter = ("name", "email")
 
 
+class PricingAdmin(admin.ModelAdmin):
+    list_display = ("name", "min_population", "max_population", "pricing")
+    list_filter = ("name", "min_population", "max_population", "pricing")
+
+
 admin.site.register(Agency, AgencyAdmin)
+admin.site.register(Pricing, PricingAdmin)
