a10dd56eba330de8309e3e11b3a221e92e9487ed
Seraphin <svandegar@hotmail.com>
2022-03-30 09:18:16 -0300

    Bump cache version

--- config/settings/production.py
+++ config/settings/production.py
@@ -26,6 +26,6 @@ CACHES = {
         "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": os.getenv("REDIS_URL"),
         "OPTIONS": {"CLIENT_CLASS": "django_redis.client.DefaultClient"},
-        "KEY_PREFIX": "estimer_v2.1.8",
+        "KEY_PREFIX": "estimer_v2.1.9",
     }
 }
