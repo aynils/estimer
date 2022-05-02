9a6287a9b3348da79766f7b5fb5d0a8b2df2656f
ExoCinki <aolacot@gmail.com>
2022-05-02 22:37:02 +0200

    Change import script for range to for in

--- src/dvf/aggregator/import_close_commune.py
+++ src/dvf/aggregator/import_close_commune.py
@@ -10,7 +10,7 @@ path_csv_out_file = CSV_DIR / 'commune_voisine.csv'
 
 
 def scrap_close_commune():
-    path_csv_code_postal = CSV_DIR / 'liste_code_postal_estimer.csv'  # Code Postal récupérer de la BDD Estimer.com
+    path_csv_code_postal = CSV_DIR / 'liste_code_postal_test.csv'  # Code Postal récupérer de la BDD Estimer.com
     code_postal_df = pd.read_csv(path_csv_code_postal, usecols=["code_postal"])
     for codepostal in code_postal_df['code_postal']:
         if codepostal <= 9999:
@@ -25,8 +25,8 @@ def scrap_close_commune():
             a = r.json()
             commune_voisines = []
             if type(a) == list:
-                for i in range(len(a)):
-                    commune_voisine = CommuneVoisine(code_postal_a=cp, code_postal_b=a[i]['code_postal'], distance=a[i]['distance'])
+                for i in a:
+                    commune_voisine = CommuneVoisine(code_postal_a=cp, code_postal_b=i['code_postal'], distance=i['distance'])
                     commune_voisines.append(commune_voisine)
             else:
                 filtered_json = {commune.get('code_postal'): commune.get('distance') for commune in a.values()}
